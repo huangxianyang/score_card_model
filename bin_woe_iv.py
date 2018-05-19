51e34c7cae32cdc0739f6b857ccf090d923668d5
Mr.Cat <hxysir@163.com>
2018-05-19 20:32:56 +0800

    Update bin_woe_iv.py

--- bin_woe_iv.py
+++ bin_woe_iv.py
@@ -3,8 +3,31 @@
 连续变量最优分箱
 分类变量最优降基
 特征信息价值 Get_IV()
-特征权重转换
+特征权重转换 Woe_Trans()
 """
+def Woe_Trans(df,good=0,bad=1,y='y'):
+    """
+    输出woe转换映射
+    -------------------------
+    parameter:
+            df: dataframe, must include columns y
+            good: int, good label ,target label 
+            bad: int,bad label,target label
+            y: str,target columns name
+    return:
+            woe_maps : dict key is feature name,value is feature classifer woe
+    """
+    
+    feature_name = list(df.drop(y,axis=1).columns)
+    woe_maps = dict()
+    for i in feature_name:
+        col_good_ratio = df.loc[df[y]==good,i].value_counts()/len(df[df[y]==good]) #各分类好客户比例
+        col_bad_ratio = df.loc[df[y]==bad,i].value_counts()/len(df[df[y]==bad]) #各分类坏客户比例
+        col_good_bad_ratio = col_good_ratio/col_bad_ratio #各分类好坏比
+        woe = -np.log(col_good_bad_ratio) #各分类特征权重
+        woe_maps[i] = woe.to_dict()
+    return woe_maps
+
 
 def Get_IV(df,good=0,bad=1,y='y'):
     """
